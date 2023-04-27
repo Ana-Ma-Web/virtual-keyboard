a801bfbd572e95dae054693d3d7897a167ada7a1
Ana-Ma-Web <90105049+Ana-Ma-Web@users.noreply.github.com>
2023-04-27 15:06:17 +0300

    feat: add keyboard generation

---
+++ src/js/index.js
@@ -0,0 +1,793 @@
+const data = {
+  isShift: false,
+  isRightAlt: false,
+  isBackquote: false,
+  isShiftBackquote: false,
+  isQuote: false,
+  isAltQuote: false,
+  isAltBackslash: false,
+  lang: 'ice',
+  icelandic: [
+    {
+      code: 'Backquote',
+      key: '°',
+      shiftKey: '¨',
+    },
+    {
+      code: 'Digit1',
+      key: '1',
+      shiftKey: '!',
+    },
+    {
+      code: 'Digit2',
+      key: '2',
+      shiftKey: '"',
+    },
+    {
+      code: 'Digit3',
+      key: '3',
+      shiftKey: '#',
+    },
+    {
+      code: 'Digit4',
+      key: '4',
+      shiftKey: '$',
+    },
+    {
+      code: 'Digit5',
+      key: '5',
+      shiftKey: '%',
+      rightAlt: '€',
+    },
+    {
+      code: 'Digit6',
+      key: '6',
+      shiftKey: '&',
+    },
+    {
+      code: 'Digit7',
+      key: '7',
+      shiftKey: '/',
+      rightAlt: '€',
+    },
+    {
+      code: 'Digit8',
+      key: '8',
+      shiftKey: '¨',
+      rightAlt: '{',
+    },
+    {
+      code: 'Digit9',
+      key: '9',
+      shiftKey: ')',
+      rightAlt: ']',
+    },
+    {
+      code: 'Digit0',
+      key: '0',
+      shiftKey: '=',
+      rightAlt: '}',
+    },
+    {
+      code: 'Minus',
+      key: 'ö',
+      shiftKey: 'Ö',
+      rightAlt: '&#92',
+    },
+    {
+      code: 'Equal',
+      key: '-',
+      shiftKey: '_',
+    },
+    {
+      code: 'Backspace',
+      key: 'Backspace',
+    },
+    {
+      code: 'Tab',
+      key: 'Tab',
+    },
+    {
+      code: 'KeyQ',
+      key: 'q',
+      shiftKey: 'Q',
+      rightAlt: '@',
+    },
+    {
+      code: 'KeyW',
+      key: 'w',
+      shiftKey: 'W',
+    },
+    {
+      code: 'KeyE',
+      key: 'e',
+      shiftKey: 'E',
+      rightAlt: '€',
+      shiftBackquote: 'ë',
+      shiftBackquoteShift: 'Ë',
+      quote: 'é',
+      quoteShift: 'É',
+      altQuote: 'ê',
+      altQuoteShift: 'Ê',
+      altBackslash: 'è',
+      altBackslashShift: 'È',
+    },
+    {
+      code: 'KeyR',
+      key: 'r',
+      shiftKey: 'R',
+    },
+    {
+      code: 'KeyT',
+      key: 't',
+      shiftKey: 'T',
+    },
+    {
+      code: 'KeyY',
+      key: 'y',
+      shiftKey: 'Y',
+      shiftBackquote: 'ÿ',
+      shiftBackquoteShift: 'Ÿ',
+      quote: 'ý',
+      quoteShift: 'Ý',
+    },
+    {
+      code: 'KeyU',
+      key: 'u',
+      shiftKey: 'U',
+      shiftBackquote: 'ü',
+      shiftBackquoteShift: 'Ü',
+      quote: 'ú',
+      quoteShift: 'Ú',
+      altQuote: 'û',
+      altQuoteShift: 'Û',
+      altBackslash: 'ù',
+      altBackslashShift: 'Ù',
+    },
+    {
+      code: 'KeyI',
+      key: 'i',
+      shiftKey: 'I',
+      shiftBackquote: 'ï',
+      shiftBackquoteShift: 'Ï',
+      quote: 'í',
+      quoteShift: 'Í',
+      altQuote: 'î',
+      altQuoteShift: 'Î',
+      altBackslash: 'ì',
+      altBackslashShift: 'Ì',
+    },
+    {
+      code: 'KeyO',
+      key: 'o',
+      shiftKey: 'O',
+      shiftBackquote: 'ö',
+      shiftBackquoteShift: 'Ö',
+      quote: 'ó',
+      quoteShift: 'Ó',
+      altQuote: 'ô',
+      altQuoteShift: 'Ô',
+      altBackslash: 'ò',
+      altBackslashShift: 'Ò',
+    },
+    {
+      code: 'KeyP',
+      key: 'p',
+      shiftKey: 'P',
+    },
+    {
+      code: 'BracketLeft',
+      key: 'ð',
+      shiftKey: 'Ð',
+    },
+    {
+      code: 'BracketRight',
+      key: "'",
+      shiftKey: '?',
+      rightAlt: '~',
+    },
+    {
+      code: 'Backslash',
+      key: '+',
+      shiftKey: '*',
+      rightAlt: '`',
+    },
+    {
+      code: 'Delete',
+      key: 'Del',
+    },
+    {
+      code: 'CapsLock',
+      key: 'CapsLock',
+    },
+    {
+      code: 'KeyA',
+      key: 'a',
+      shiftKey: 'A',
+      backquote: 'å',
+      backquoteShift: 'Å',
+      shiftBackquote: 'ä',
+      shiftBackquoteShift: 'Ä',
+      quote: 'á',
+      quoteShift: 'Á',
+      altQuote: 'â',
+      altQuoteShift: 'Â',
+      altBackslash: 'à',
+      altBackslashShift: 'À',
+    },
+    {
+      code: 'KeyS',
+      key: 's',
+      shiftKey: 'S',
+    },
+    {
+      code: 'KeyD',
+      key: 'd',
+      shiftKey: 'D',
+    },
+    {
+      code: 'KeyF',
+      key: 'f',
+      shiftKey: 'F',
+    },
+    {
+      code: 'KeyG',
+      key: 'g',
+      shiftKey: 'G',
+    },
+    {
+      code: 'KeyH',
+      key: 'h',
+      shiftKey: 'H',
+    },
+    {
+      code: 'KeyJ',
+      key: 'j',
+      shiftKey: 'J',
+    },
+    {
+      code: 'KeyK',
+      key: 'k',
+      shiftKey: 'K',
+    },
+    {
+      code: 'KeyL',
+      key: 'l',
+      shiftKey: 'L',
+    },
+    {
+      code: 'Semicolon',
+      key: 'æ',
+      shiftKey: 'Æ',
+    },
+    {
+      code: 'Quote',
+      key: '&#180;',
+      rightAlt: '^',
+    },
+    {
+      code: 'Enter',
+      key: 'Enter',
+    },
+    {
+      code: 'ShiftLeft',
+      key: 'Shift',
+    },
+    {
+      code: 'KeyZ',
+      key: 'z',
+      shiftKey: 'Z',
+    },
+    {
+      code: 'KeyX',
+      key: 'x',
+      shiftKey: 'X',
+    },
+    {
+      code: 'KeyC',
+      key: 'c',
+      shiftKey: 'C',
+    },
+    {
+      code: 'KeyV',
+      key: 'v',
+      shiftKey: 'V',
+    },
+    {
+      code: 'KeyB',
+      key: 'b',
+      shiftKey: 'B',
+    },
+    {
+      code: 'KeyN',
+      key: 'n',
+      shiftKey: 'N',
+    },
+    {
+      code: 'KeyM',
+      key: 'm',
+      shiftKey: 'M',
+      rightAlt: 'µ',
+    },
+    {
+      code: 'Comma',
+      key: ',',
+      shiftKey: ';',
+    },
+    {
+      code: 'Period',
+      key: '.',
+      shiftKey: ':',
+    },
+    {
+      code: 'Slash',
+      key: 'Þ',
+      shiftKey: 'Þ',
+    },
+    {
+      code: 'ArrowUp',
+      key: '&uarr;',
+    },
+    {
+      code: 'ShiftRight',
+      key: 'Shift',
+    },
+    {
+      code: 'ControlLeft',
+      key: 'Ctrl',
+    },
+    {
+      code: 'MetaLeft',
+      key: 'Win',
+    },
+    {
+      code: 'AltLeft',
+      key: 'Alt',
+    },
+    {
+      code: 'Space',
+      key: ' ',
+    },
+    {
+      code: 'AltRight',
+      key: 'Alt',
+    },
+    {
+      code: 'ArrowLeft',
+      key: '&larr;',
+    },
+    {
+      code: 'ArrowDown',
+      key: '&darr;',
+    },
+    {
+      code: 'ArrowRight',
+      key: '&rarr;',
+    },
+    {
+      code: 'ControlRight',
+      key: 'Ctrl',
+    },
+  ],
+  english: [
+    { isBackquote: false },
+    { isShiftBackquote: false },
+    { isQuote: false },
+    { isAltQuote: false },
+    { isAltBackslash: false },
+    {
+      code: 'Backquote',
+      key: '`',
+      shiftKey: '~',
+    },
+    {
+      code: 'Digit1',
+      key: '1',
+      shiftKey: '!',
+    },
+    {
+      code: 'Digit2',
+      key: '2',
+      shiftKey: '@',
+    },
+    {
+      code: 'Digit3',
+      key: '3',
+      shiftKey: '#',
+    },
+    {
+      code: 'Digit4',
+      key: '4',
+      shiftKey: '$',
+    },
+    {
+      code: 'Digit5',
+      key: '5',
+      shiftKey: '%',
+    },
+    {
+      code: 'Digit6',
+      key: '6',
+      shiftKey: '^',
+    },
+    {
+      code: 'Digit7',
+      key: '7',
+      shiftKey: '&',
+    },
+    {
+      code: 'Digit8',
+      key: '8',
+      shiftKey: '*',
+    },
+    {
+      code: 'Digit9',
+      key: '9',
+      shiftKey: '(',
+    },
+    {
+      code: 'Digit0',
+      key: '0',
+      shiftKey: ')',
+    },
+    {
+      code: 'Minus',
+      key: '-',
+      shiftKey: '_',
+    },
+    {
+      code: 'Equal',
+      key: '=',
+      shiftKey: '+',
+    },
+    {
+      code: 'Backspace',
+      key: 'Backspace',
+    },
+    {
+      code: 'Tab',
+      key: 'Tab',
+    },
+    {
+      code: 'KeyQ',
+      key: 'q',
+      shiftKey: 'Q',
+    },
+    {
+      code: 'KeyW',
+      key: 'w',
+      shiftKey: 'W',
+    },
+    {
+      code: 'KeyE',
+      key: 'e',
+      shiftKey: 'E',
+    },
+    {
+      code: 'KeyR',
+      key: 'r',
+      shiftKey: 'R',
+    },
+    {
+      code: 'KeyT',
+      key: 't',
+      shiftKey: 'T',
+    },
+    {
+      code: 'KeyY',
+      key: 'y',
+      shiftKey: 'Y',
+    },
+    {
+      code: 'KeyU',
+      key: 'u',
+      shiftKey: 'U',
+    },
+    {
+      code: 'KeyI',
+      key: 'i',
+      shiftKey: 'I',
+    },
+    {
+      code: 'KeyO',
+      key: 'o',
+      shiftKey: 'O',
+    },
+    {
+      code: 'KeyP',
+      key: 'p',
+      shiftKey: 'P',
+    },
+    {
+      code: 'BracketLeft',
+      key: '[',
+      shiftKey: '{',
+    },
+    {
+      code: 'BracketRight',
+      key: ']',
+      shiftKey: '}',
+    },
+    {
+      code: 'Backslash',
+      key: '&#92',
+      shiftKey: '|',
+    },
+    {
+      code: 'Delete',
+      key: 'Del',
+    },
+    {
+      code: 'CapsLock',
+      key: 'CapsLock',
+    },
+    {
+      code: 'KeyA',
+      key: 'a',
+      shiftKey: 'A',
+    },
+    {
+      code: 'KeyS',
+      key: 's',
+      shiftKey: 'S',
+    },
+    {
+      code: 'KeyD',
+      key: 'd',
+      shiftKey: 'D',
+    },
+    {
+      code: 'KeyF',
+      key: 'f',
+      shiftKey: 'F',
+    },
+    {
+      code: 'KeyG',
+      key: 'g',
+      shiftKey: 'G',
+    },
+    {
+      code: 'KeyH',
+      key: 'h',
+      shiftKey: 'H',
+    },
+    {
+      code: 'KeyJ',
+      key: 'j',
+      shiftKey: 'J',
+    },
+    {
+      code: 'KeyK',
+      key: 'k',
+      shiftKey: 'K',
+    },
+    {
+      code: 'KeyL',
+      key: 'l',
+      shiftKey: 'L',
+    },
+    {
+      code: 'Semicolon',
+      key: ';',
+      shiftKey: ':¨',
+    },
+    {
+      code: 'Quote',
+      key: "'",
+      rightAlt: '"',
+    },
+    {
+      code: 'Enter',
+      key: 'Enter',
+    },
+    {
+      code: 'ShiftLeft',
+      key: 'Shift',
+    },
+    {
+      code: 'KeyZ',
+      key: 'z',
+      shiftKey: 'Z¨',
+    },
+    {
+      code: 'KeyX',
+      key: 'x',
+      shiftKey: 'X',
+    },
+    {
+      code: 'KeyC',
+      key: 'c',
+      shiftKey: 'C',
+    },
+    {
+      code: 'KeyV',
+      key: 'v',
+      shiftKey: 'V',
+    },
+    {
+      code: 'KeyB',
+      key: 'b',
+      shiftKey: 'B',
+    },
+    {
+      code: 'KeyN',
+      key: 'n',
+      shiftKey: 'N',
+    },
+    {
+      code: 'KeyM',
+      key: 'm',
+      shiftKey: 'M',
+    },
+    {
+      code: 'Comma',
+      key: ',',
+      shiftKey: '<',
+    },
+    {
+      code: 'Period',
+      key: '.',
+      shiftKey: '>',
+    },
+    {
+      code: 'Slash',
+      key: '/',
+      shiftKey: '?',
+    },
+    {
+      code: 'ArrowUp',
+      key: '&uarr;',
+    },
+    {
+      code: 'ShiftRight',
+      key: 'Shift',
+    },
+    {
+      code: 'ControlLeft',
+      key: 'Ctrl',
+    },
+    {
+      code: 'MetaLeft',
+      key: 'Win',
+    },
+    {
+      code: 'AltLeft',
+      key: 'Alt',
+    },
+    {
+      code: 'Space',
+      key: ' ',
+    },
+    {
+      code: 'AltRight',
+      key: 'Alt',
+    },
+    {
+      code: 'ArrowLeft',
+      key: '&larr;',
+    },
+    {
+      code: 'ArrowDown',
+      key: '&darr;',
+    },
+    {
+      code: 'ArrowRight',
+      key: '&rarr;',
+    },
+    {
+      code: 'ControlRight',
+      key: 'Ctrl',
+    },
+  ],
+};
+
+const renderWrapper = () => {
+  const wrapper = `<div class="wrapper">
+    <div class="main">
+      <div class="textboard">
+        <textarea 
+        class="textboard__text-area" 
+        name="text" 
+        cols="72" 
+        rows="4">Þat mælti mín móðir: at mér skyldi kaupa</textarea>
+      </div>
+      <div class="keyboard" data-lang="${data.lang}"></div>
+    </div>
+  </div>`;
+  document.body.innerHTML = wrapper;
+};
+
+class Key {
+  constructor(obj) {
+    this.code = obj.code;
+    this.key = obj.key;
+    this.shiftKey = obj.shiftKey;
+    this.rightAlt = obj.rightAlt;
+    this.backquote = obj.backquote;
+    this.backquoteShift = obj.backquoteShift;
+    this.shiftBackquote = obj.shiftBackquote;
+    this.shiftBackquoteShift = obj.shiftBackquoteShift;
+    this.quote = obj.quote;
+    this.quoteShift = obj.quoteShift;
+    this.altQuote = obj.altQuote;
+    this.altQuoteShift = obj.altQuoteShift;
+    this.altBackslash = obj.altBackslash;
+    this.altBackslashShift = obj.altBackslashShift;
+  }
+
+  generateKey() {
+    let template = '';
+    let char = this.key;
+    const key = document.createElement('div');
+    key.className = 'key';
+    key.setAttribute('data-code', this.code);
+
+    if (data.isShift) {
+      char = this.shiftKey ? this.shiftKey : char;
+      if (data.isBackquote) {
+        char = this.backquoteShift ? this.backquoteShift : char;
+      }
+      if (data.isShiftBackquote) {
+        char = this.shiftBackquoteShift ? this.shiftBackquoteShift : char;
+      }
+      if (data.isQuote) {
+        char = this.quoteShift ? this.quoteShift : char;
+      }
+      if (data.isAltQuote) {
+        char = this.altQuoteShift ? this.altQuoteShift : char;
+      }
+      if (data.isAltBackslash) {
+        char = this.altBackslashShift ? this.altBackslashShift : char;
+      }
+    } else if (data.isRightAlt) {
+      char = this.rightAlt ? this.rightAlt : char;
+    }
+
+    template += `<button class="key__button">
+    <div class="key__bg key__bg_primary"></div>
+    <div class="key__bg key__bg_active"></div>
+    <span class="key__text">${char}</span>
+  </button>`;
+
+    key.innerHTML = template;
+    return key;
+  }
+}
+
+const generateKeys = (arr) => {
+  const keys = [];
+  arr.forEach((e) => {
+    keys.push(new Key(e));
+  });
+  return keys;
+};
+
+const renderKeyboard = () => {
+  const arr = ((data.lang === 'ice')
+    ? data.icelandic : data.english);
+  const keyboard = document.querySelector('.keyboard');
+  generateKeys(arr).forEach((e) => {
+    if (e.code) {
+      keyboard.append(e.generateKey());
+    }
+  });
+};
+
+const addKeyCkickHandler = () => {
+  // const doc = document.body;
+  // doc.addEventListener('keydown', (e) => {
+  // data.icelandic.push({ [e.code]: e.key });
+  // console.log(data.icelandic);
+  // });
+};
+
+const initKeyboard = () => {
+  document.body.innerHTML = '';
+  renderWrapper();
+  renderKeyboard();
+  addKeyCkickHandler();
+};
+
+window.onload = function () {
+  initKeyboard();
+};
